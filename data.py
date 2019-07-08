387bef08974d0c8425f23c780280a2505858f2bc
1Dennis Hosea <dennisodwaro@gmail.com>
2019-07-08 06:23:14 +0300

    added content to a file

---
+++ data.py
@@ -0,0 +1,24 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'The crying bood, sadness and anger, the weeping shore'
+            'author':'Dennis Hosea',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'The crying bood, sadness and anger, the weeping shore'
+            'author':'Dennis Hosea',
+            'create_date':'04-25-2019'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'The crying bood, sadness and anger, the weeping shore'
+            'author':'Dennis Hosea',
+            'create_date':'04-25-2019'
+    ]
+    return articles
